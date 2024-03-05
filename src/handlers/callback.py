eae6070f4c02b8866b67772be4c1b3e0849db1d5
Otaboyev Sardorbek <127109664+otaboyevsardorbek1@users.noreply.github.com>
2024-03-05 18:44:03 +0500

    improve

--- src/handlers/callback.py
+++ src/handlers/callback.py
@@ -5,31 +5,30 @@ from aiogram.utils.callback_data import CallbackData
 from src.data.state.repeat_state import RepeatState
 from src.lib.anonymouse import AnonyMouse
 
+posts_cb = CallbackData('post', 'id', 'action')
 
 def register_callback_handler(dp: Dispatcher, bot: Bot, anonymouse: AnonyMouse):
-    posts_cb: CallbackData = CallbackData('post', 'id', 'action')
-
     @dp.callback_query_handler(posts_cb.filter(action=['send']))
-    async def send(query: types.CallbackQuery, state: FSMContext):
-        async with state.proxy() as data:
-            try:
-                await anonymouse.send_email_message(data["mail"], data["title"], data["body"])
-                await bot.send_message(query.message.chat.id, "message was sent")
-            except KeyError as e:
-                print(e)
+    async def send_email(query: types.CallbackQuery, state: FSMContext):
+        data = await state.get_data()
+        try:
+            await anonymouse.send_email_message(data["mail"], data["title"], data["body"])
+            await bot.send_message(query.message.chat.id, "message was sent")
+        except KeyError as e:
+            print(e)
         await state.finish()
 
     @dp.callback_query_handler(posts_cb.filter(action=["edit_title"]))
-    async def send(query: types.CallbackQuery):
+    async def edit_title(query: types.CallbackQuery):
         await RepeatState.repeat_title.set()
         await bot.send_message(query.message.chat.id, "send new title")
 
     @dp.callback_query_handler(posts_cb.filter(action=["edit_body"]))
-    async def send(query: types.CallbackQuery):
-        await RepeatState.repeat_title.set()
+    async def edit_body(query: types.CallbackQuery):
+        await RepeatState.repeat_body.set()
         await bot.send_message(query.message.chat.id, "send new body")
 
     @dp.callback_query_handler(posts_cb.filter(action=["edit_mail"]))
-    async def send(query: types.CallbackQuery, state: FSMContext):
-        await RepeatState.repeat_title.set()
+    async def edit_mail(query: types.CallbackQuery, state: FSMContext):
+        await RepeatState.repeat_mail.set()
         await bot.send_message(query.message.chat.id, "send new mail")
